8a24f6b2d8e5e243b8766da005a25965935ccc2b
Yuri M <yurisdraughts@gmail.com>
2022-08-02 20:09:13 +0300

    Added random numbers

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,3 +1,5 @@
-﻿double numberA = 12;
-double numberB = 5;
-Console.WriteLine(numberA / numberB);
+﻿int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA + numberB);
