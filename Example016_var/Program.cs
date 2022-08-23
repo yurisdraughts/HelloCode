73f22b6a770ba59250f4c29d12b326d1e568583c
Yuri M <yurisdraughts@gmail.com>
2022-08-23 19:46:21 +0300

    added Example016_var

---
+++ Example016_var/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+var data = new int[] {1,2,3,4}
+           .Where(e => e > 0)
+           .Select(e => new {q = e, w = e+1});
+
+Console.WriteLine(data.GetType());
+//System.Linq.Enumerable+WhereSelectArrayIterator`2[System.Int32,<>f__AnonymousType0`2[System.Int32,System.Int32]]
+//подобный тип невозможно задать иначе, чем через var
